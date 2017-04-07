603ae607d7822b16d6f42ed06026701422cf8400
clement <clement@164.81.232.80>
2017-04-07 17:16:26 +0200

    fizzbuzz

--- src/main/java/fr/unilim/iut/testgit/HelloWorld.java
+++ src/main/java/fr/unilim/iut/testgit/HelloWorld.java
@@ -1,5 +1,17 @@
 package fr.unilim.iut.testgit;
 
 public class HelloWorld {
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Jouons ensembles");
+        System.out.println( "Et si on jouait au FizzBuzz ?" );  
+        System.out.println(FizzBuzz.getResult());
+    }
+	 public static class FizzBuzz {
 
+		    public static String getResult() {
+			    return "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz";
+		    }
+	 } 
 }
